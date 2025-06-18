826e78f95cf0116498b6475424c937aa1cd3454d
Ehtisham <137653528+shambat@users.noreply.github.com>
2025-06-18 21:47:41 +0500

    Add files via upload

---
+++ backup/db_backup.sql
@@ -0,0 +1,7 @@
+-- Haha! Not a real database.
+
+-- This file was placed here intentionally for curious minds.
+-- You're doing a great job with your recon, but there's nothing sensitive here.
+
+-- Remember: Hack ethically.
+-- Muhammad Ehtisham | ehtisham.space
